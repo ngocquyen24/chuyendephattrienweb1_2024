4f69fe0bb263efb31a54e0817400570358833078
ngocquyen <ngocquyencv2468@gmail.com>
2024-09-24 08:22:52 +0700

    source oop

---
+++ oop/MyInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+interface IA {
+  public function NotDeclare();
+}
+
+interface IB {
+   
+
+}
+interface IC{
+   
+
+}
+
+interface Interface1{
+  function func_from_Int1();
+}
+interface Interface2{
+  function func_from_Int1();
+}
+interface Interface3{
+  function func_from_Int1();
+}
+
+// interface Interface2 {
+
+// }
+
+// interface Interface3 {
+
+// }
